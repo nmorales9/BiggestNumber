e5ae9d4e3fdedbc45b42b162115f228a71b34b1b
nmorales9 <nmorales9@mail.csuchico.edu>
2018-02-06 11:37:08 -0800

    description of what this version does

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author:Nysa Morales
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
@@ -7,10 +7,26 @@ int main()
 {
   
   cout<<"Please enter a whole number:\n";
+
+  int wholenumber1;
+  cin>> wholenumber1;
   
   cout<<"Please enter another whole number:\n";
+  
+  int wholenumber2;
+  cin>> wholenumber2;
 
   cout<<"Of those two numbers, the biggest is: ";
+
+  if (wholenumber1 >= wholenumber2)
+  {
+    cout<< wholenumber1;
+  }
+  else
+  {
+  cout<< wholenumber2;
+  }
+
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
